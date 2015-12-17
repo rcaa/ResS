34ba2e8e4971eed0ac8c49513a0113467e702d02
João Pedro de Medeiros Santos <jpms2@cin.ufpe.br>
2015-12-16 21:13:23 -0300

    Update GebConfig.groovy

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -3,24 +3,18 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
 
 driver = {
-//def driver = new HtmlUnitDriver()
-//driver.javascriptEnabled = true
-//driver
-def driver = new FirefoxDriver()
-driver
+ File file = new File("chromedrivers/chromedriverlinux64"); //configurar com o enderço correto do chromedriver.
+    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+    new ChromeDriver();
 } 
-
-baseUrl = "http://localhost:8080/"
 
 environments {
    // run as “grails -Dgeb.env=chrome test-app”
    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
    chrome {
-     driver = {
-    File file = new File("chromedrivers/chromedriverlinux64"); //configurar com o enderço correto do chromedriver.
-    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-    new ChromeDriver();
-}
+      driver = { File file = new File("chromedrivers/chromedriverlinux64"); //configurar com o enderço correto do chromedriver.
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+            new ChromeDriver(); }
    }
 
    // run as “grails -Dgeb.env=firefox test-app”
