32344d3813c292c40fbe5e6f9882829a752fa8f5
pc-uag <pc-uag@email.com>
2017-02-22 15:52:50 -0300

    Adicionado teste de GUI - Alysson

--- test/cucumber/HistoricoDeColeta.feature
+++ test/cucumber/HistoricoDeColeta.feature
@@ -2,6 +2,12 @@ Feature: Gerar historico de coleta
   As a membro da empresa de coleta
   I want to gerar um historico de coletas
   So that eu posso saber o desempenho da minha empresa
+ 
+ 
+ Scenario: adicionar coleta com dados invalidos em volume
+ Given estou na pagina de adicionar coleta
+ When preencho o campo volume com valor "dois"
+ Then eu vejo uma menssagem de erro
 
 @ignore
   Scenario: adicionar coleta ja existente
--- test/functional/pages/HistoricoPage.groovy
+++ test/functional/pages/HistoricoPage.groovy
@@ -11,6 +11,15 @@ class HistoricoPage extends Page{
 
         title ==~ titulo
     }
+	
+	def fillColetaInfo(String volume){
+		$("form").nome = "RU"
+		$("form").data_day = "8"
+		$("form").data_month = "4"
+		$("form").data_year = "2015"
+		$("form").volume = volume
+
+	}
 
     def fillColetaInfo(){
         $("form").nome = "RU"
@@ -24,6 +33,18 @@ class HistoricoPage extends Page{
     def selectAdicionarColeta(){
         $("input", name: "create").click()
     }
+	
+
+	def boolean hasInvalidMessage(){
+		
+				def invalidField = $('input:invalid')
+		
+				if(invalidField != null){
+					return true
+				} else {
+					return false
+				}
+	}
 
 }
 
