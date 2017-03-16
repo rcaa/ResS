46cc16d5b3767b5e07528695174ccb966e83c5b2
Djeymisson <djeymisson@gmail.com>
2017-03-16 09:13:57 -0300

    Corrigido alguns erros decorrentes do merge.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -63,31 +63,6 @@ class BootStrap {
 			harvestSolicitation: null
 		)
 
-
-
-		def stockResidueGenerator3 = new ResidueGenerator(
-			nameGenerator: "Nuka Cola",
-			type: "Restaurante",
-			cnpj: "35401447000157",
-			addressGenerator: "Elm Street number 15",
-			averageDailyMeals: 20,
-			averageMonthlyMeals: 1000,
-			hasActiveHarvest: false,
-			harvestSolicitation: null
-		)
-		
-		def stockResidueGenerator4 = new ResidueGenerator(
-			nameGenerator: "Boa Vista",
-			type: "Restaurante",
-			cnpj: "35401447000157",
-			addressGenerator: "Elm Street number 16",
-			averageDailyMeals: 20,
-			averageMonthlyMeals: 1000,
-			hasActiveHarvest: false,
-			harvestSolicitation: null
-		)
-
-
         ru.save(failOnError: true)
         stockResidueGenerator.save(failOnError: true)
         stockResidueGenerator2.save(failOnError: true)
