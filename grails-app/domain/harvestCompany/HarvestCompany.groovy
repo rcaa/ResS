fe2f2467877241c195b22293dc25353b35a8174b
Djeymisson <djeymisson@gmail.com>
2017-03-16 21:36:44 -0300

    Corrigido alguns erros apontados pelo CodeNarc

--- grails-app/domain/harvestCompany/HarvestCompany.groovy
+++ grails-app/domain/harvestCompany/HarvestCompany.groovy
@@ -11,4 +11,33 @@ class HarvestCompany {
         id nullable:false,unique:true,blank:false
         name nullable: false, blank: false
     }
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HarvestCompany other = (HarvestCompany) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
 }
