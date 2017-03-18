0eb31e87a404d87aae0033582e2a000e4581033b
NatalicioCavalcante <nataliciocavalcanteabraga@gmail.com>
2017-03-17 21:36:19 -0300

    Função deletar da nova funcionalidade do sistema...

---
+++ grails-app/controllers/user/UserController.groovy
@@ -0,0 +1,39 @@
+package user
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class UserController {
+
+    static allowedMethods = [delete: "DELETE"]
+
+    @Transactional
+    def delete(User userInstance) {
+
+        if (userInstance == null) {
+            notFound()
+            return
+        }
+
+        userInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'User.label', default: 'User'), userInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
