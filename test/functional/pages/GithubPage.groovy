c038f38acdc0a7612d0e90396daa626c58614da6
NatalicioCavalcante <nataliciocavalcanteabraga@gmail.com>
2017-03-18 23:54:27 -0300

    Resolvi o erro do teste da feature HomePage, onde o mesmo agora consegue completar o teste de acesso ao repositorio ReeS - Paulo Borba no Git Hub...

--- test/functional/pages/GithubPage.groovy
+++ test/functional/pages/GithubPage.groovy
@@ -4,7 +4,7 @@ import geb.Page
 
 class GithubPage extends Page {
 
-    def titulo = "pauloborba/ResS · GitHub"
+    def titulo = "GitHub - pauloborba/ResS: residue management system"
     static url = "https://github.com/pauloborba/ResS"
 
     static at = {
