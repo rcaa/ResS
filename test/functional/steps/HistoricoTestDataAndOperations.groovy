90aa9768ba849e12157f117fd9eaad95047f54f6
Djeymisson <djeymisson@gmail.com>
2017-03-20 16:10:42 -0300

    Efetuado refatoração, extraindo método em comum.

--- test/functional/steps/HistoricoTestDataAndOperations.groovy
+++ test/functional/steps/HistoricoTestDataAndOperations.groovy
@@ -50,33 +50,29 @@ class HistoricoTestDataAndOperations {
 		cont.response.reset()
 	}
 
-    static public void editColeta(int volume,Coleta coleta){
+    static public void editVolumeColeta(int volume,Coleta coleta){
         def colet = coleta
         colet.setVolume(volume)
-        def cont =  new ColetaController()
-        cont.params << colet.properties
-        cont.update()
+        editColeta(colet)
     }
 
     static public void editNomeColeta(String novoNome,Coleta coleta){
         def colet = coleta
         colet.setName(novoNome)
-        def cont =  new ColetaController()
-        cont.params << colet.properties
-        cont.update()
-
-
+        editColeta(colet)
     }
 
     static public void editDataColeta(@Format("dd/MM/yyyy") Date novaData,Coleta coleta){
         def colet = coleta
         colet.setDate(novaData)
-        def cont =  new ColetaController()
-        cont.params << colet.properties
-        cont.update()
-
-
-    }
+        editColeta(colet)
+    } 
+	
+	static public void editColeta(def colet){
+		def cont =  new ColetaController()
+		cont.params << colet.properties
+		cont.update()
+	}
 
     static public void deleteColeta(Coleta coleta){
         def cont = new ColetaController()
